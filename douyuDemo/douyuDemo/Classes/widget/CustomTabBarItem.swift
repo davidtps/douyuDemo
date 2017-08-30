9c2381c49de57a50e37a813d10bde40b45989e56
田鹏升 <tianpengsheng@tianpengshengdemac-mini.local>
2017-08-30 19:28:54 +0800

    9.0 以上  首页tabbar的实现方式

---
+++ douyuDemo/douyuDemo/Classes/widget/CustomTabBarItem.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTabBarItem.swift
+//  douyuDemo
+//
+//  Created by 田鹏升 on 2017/8/30.
+//  Copyright © 2017年 田鹏升. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarItem: UITabBarItem {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.darkGray], for:.normal)
+        setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.orange], for: .selected)
+    
+        
+    }
+   
+}
