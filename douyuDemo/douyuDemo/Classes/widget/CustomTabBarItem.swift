6b0e035f4991456672ffc5114ad5b3924fed223a
田鹏升 <tianpengsheng@tianpengshengdemac-mini.local>
2017-08-31 16:48:16 +0800

    导航栏的图标展示，使用两种方式添加图标；1.代码罗列  2.通过扩展类方法的形式来实现

--- douyuDemo/douyuDemo/Classes/widget/CustomTabBarItem.swift
+++ douyuDemo/douyuDemo/Classes/widget/CustomTabBarItem.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
-
+// MARK:- 自定义  tabbar item
 class CustomTabBarItem: UITabBarItem {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
         setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.darkGray], for:.normal)
         setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.orange], for: .selected)
-    
+        
         
     }
-   
+    
+    
+    
 }
